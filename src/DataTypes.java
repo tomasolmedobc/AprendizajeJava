58062defdc221dad3c02cd215d02c4eff5258d1c
Tomas Olmedo <tomasolmedobc@gmail.com>
2022-08-16 12:08:04 -0300

    Utilizando Java V11

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -6,5 +6,16 @@ public class DataTypes {
         double  nD   = 123.456;
         float   nF   = 123.4577F;
 
+        //
+        var salary = 1000;
+        //pension 3%
+        var pension = salary*0.03;
+        var totalSalary = salary - pension;
+        System.out.println(salary);
+        System.out.println(pension);
+        System.out.println(totalSalary);
+
+        var employeeName = "Tomas Olmedo";
+        System.out.println("Employee: " + employeeName + " SALARY: " + totalSalary);
     }
 }
