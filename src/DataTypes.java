eb9b0a4539ae3191d718e7db01d0d3869b0534b8
Tomas Olmedo <tomasolmedobc@gmail.com>
2022-08-16 11:50:25 -0300

    Utilizando Java V11

---
+++ src/DataTypes.java
@@ -0,0 +1,10 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int n   = 1312312344;
+        long nL  = 12381231317L;
+
+        double  nD   = 123.456;
+        float   nF   = 123.4577F;
+
+    }
+}
